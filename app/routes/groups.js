cdf59cb47c3119e46053563c0065b964fcfd01df
Nathan Ward <ward@digivine.com>
2014-08-08 14:48:11 -0700

    Add component design for individual VMs

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -3,17 +3,59 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return [{
-      name: 'clientnode1.hpr.com'
+      name: 'clientnode1.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }, {
-      name: 'clientnode2.hpr.com'
+      name: 'clientnode2.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }, {
-      name: 'clientnode3.hpr.com'
+      name: 'clientnode3.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }, {
-      name: 'clientnode4.hpr.com'
+      name: 'clientnode4.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }, {
-      name: 'clientnode5.hpr.com'
+      name: 'clientnode5.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }, {
-      name: 'clientnode6.hpr.com'
+      name: 'clientnode6.hpr.com',
+      vms: [{
+        name: 'test2-5bf08381-4c24-43e7-a28f-a21519f1d2f4'
+      }, {
+        name: 'test2-b676257d-418c-46d2-a195-3506d813f4fa'
+      }, {
+        name: 'test2-cd1079b6-bf38-4660-8459-de6d57022357'
+      }]
     }];
   }
 });

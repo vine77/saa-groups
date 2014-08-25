f7cb4a9b6c870eae9a0384f3cc1394a7e1007e4b
Nathan Ward <ward@digivine.com>
2014-08-25 00:18:14 -0700

    Upgrade ember-cli to 0.0.41

--- Brocfile.js
+++ Brocfile.js
@@ -7,20 +7,20 @@ var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 var trees = [];
 
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
-app.import('vendor/pnotify/pnotify.js');
-app.import('vendor/jquery-cookie/jquery.cookie.js');
-app.import('vendor/jquery-idletimer/dist/idle-timer.js');
-//app.import('vendor/d3/d3.js');
-//app.import('vendor/crossfilter/crossfilter.js');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/pnotify/pnotify.js');
+app.import('bower_components/jquery-cookie/jquery.cookie.js');
+app.import('bower_components/jquery-idletimer/dist/idle-timer.js');
+//app.import('bower_components/d3/d3.js');
+//app.import('bower_components/crossfilter/crossfilter.js');
 
-trees.push(pickFiles('vendor', {
+trees.push(pickFiles('bower_components', {
   srcDir: '/fontawesome/fonts',
   files: ['*.woff', '*.ttf', '*.svg', '*.eot'],
   destDir: '/fonts'
 }));
 
-trees.push(pickFiles('vendor', {
+trees.push(pickFiles('bower_components', {
   srcDir: '/bootstrap/fonts',
   files: ['*.woff', '*.ttf', '*.svg', '*.eot'],
   destDir: '/fonts'

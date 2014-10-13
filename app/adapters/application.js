422361040384e662d24ed685d1fbd7fc38349279
Nathan Ward <ward@digivine.com>
2014-10-13 12:29:27 -0700

    Update cgroups to api/v2 namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   // host: App.getApiDomain(),
-  namespace: 'api/v1',
+  namespace: 'api/v2',
   buildURL: function(type, id) {
     return this._super(type, id) + '.json';
   },
--- app/adapters/session.js
+++ app/adapters/session.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: 'api/v1',
+  namespace: 'api/v2',
   buildURL: function(type, id) {
     return this._super(type, id) + '.json';
   },

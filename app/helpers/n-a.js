723c8aa8d399ffffb4dbad60a9f54b0467ba41d8
Nathan Ward <ward@digivine.com>
2015-03-15 11:43:14 -0700

    Clean up Host Services templates

--- app/helpers/n-a.js
+++ app/helpers/n-a.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 import Strings from '../utils/mappings/strings';
 
-export default Ember.Handlebars.makeBoundHelper(function(value) {
-  return (Ember.isEmpty(value)) ? Strings.NA : value;
+export default Ember.Handlebars.makeBoundHelper(function (value, options) {
+  if (Ember.isEmpty(value)) return null;
+  if (value == -1) return Strings.NA;
+  var fullString = value.toString();
+  if (options.hash && options.hash.suffix) fullString = fullString + options.hash.suffix;
+  if (options.hash && options.hash.prefix) fullString = options.hash.prefix + fullString;
+  return fullString;
 });
